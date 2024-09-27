52195f048e60deb128b765aff68170ce8a0b2e18
baule <baule1321@gmail.com>
2024-09-27 16:32:26 +0700

    .

---
+++ printerpix/src/routes/index.tsx
@@ -0,0 +1,18 @@
+import { component$ } from "@builder.io/qwik";
+ 
+import {MainLayout} from "../components/pb-new/MainLayout";
+export default component$(() => {
+  return (
+   <MainLayout/>
+  );
+});
+
+// export const head: DocumentHead = {
+//   title: "Welcome to Qwik",
+//   meta: [
+//     {
+//       name: "description",
+//       content: "Qwik site description",
+//     },
+//   ],
+// };
