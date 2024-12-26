72f12b28f20751e2a5957a57f01e6b3b86d4a7eb
baudev1321 <bauledevelop1321@gmail.com>
2024-12-26 09:07:11 +0700

    update resonsive

--- src/components/pb-new/HeroSection.tsx
+++ src/components/pb-new/HeroSection.tsx
@@ -5,18 +5,18 @@ export const HeroSection = component$(() => {
   return (
     <div class="flex flex-col items-center">
       {/* Section cho Banner */}
-      <section class="relative mt-8 flex w-full flex-col items-center bg-[url('/images/banner-mobile.jpg')] bg-cover bg-center sm:bg-[url('/images/banner.jpg')] sm:bg-right">
+      <section class="relative mt-8 flex w-full flex-col items-center bg-[url('/images/banner-mobile.jpg')] bg-cover bg-center lg:bg-[url('/images/banner.jpg')] sm:bg-right">
         
         {/* Overlay với gradient đen mờ */}
-        <div class="absolute inset-0 bg-gradient-to-b from-transparent to-[rgba(0,0,0,0.6)] block md:hidden"></div>
+        <div class="absolute inset-0 bg-gradient-to-b from-transparent to-[rgba(0,0,0,0.6)] block lg:hidden"></div>
         
-        <div class="container relative flex min-h-[500px] w-full flex-col items-center justify-end md:justify-center py-10 text-sm md:min-h-[500px] md:items-start">
+        <div class="container relative flex min-h-[500px] w-full flex-col items-center justify-end lg:justify-center py-10 text-sm lg:min-h-[500px] lg:items-start">
           {/* Nội dung cho Mobile */}
-          <div class="flex w-[339px] max-w-full flex-col items-center text-center text-white sm:hidden z-10">
-            <h1 class="text-3xl font-bold leading-tight">
+          <div class="flex w-[339px] max-w-full flex-col items-center text-center text-white lg:hidden z-10">
+            <h1 class="md:text-5xl text-3xl font-bold leading-tight">
               Photo Books to Tell Your Story
             </h1>
-            <p class="mt-3 text-lg">
+            <p class="mt-3 md:text-xl text-lg">
               Starting at $14.99 <br />
               Get it by Sept 23 with Rush Shipping
             </p>
@@ -26,8 +26,8 @@ export const HeroSection = component$(() => {
           </div>
 
           {/* Nội dung cho Desktop */}
-          <div class="hidden w-[40%] max-w-full flex-col text-black sm:flex z-10">
-            <h1 class="md:text-[68px] text-[38px] font-bold leading-tight">
+          <div class="hidden w-[40%] max-w-full flex-col text-black lg:flex z-10">
+            <h1 class="xl:text-[68px] text-[48px] font-bold leading-tight">
               Photo Books to Tell Your Story
             </h1>
             <p class="mt-3 text-2xl">
